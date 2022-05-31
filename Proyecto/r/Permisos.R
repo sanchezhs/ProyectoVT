8b0311e5838d92be3f0dfe471a9b422db3b5cb4b
samuel-uma <samuelsanchez@uma.es>
2022-05-31 21:45:07 +0200

    mejoras en mi parte: grafos, matrices, intento de analizar los resultados de los av

--- Proyecto/r/Permisos.R
+++ Proyecto/r/Permisos.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(purrr)
 library(tidyverse)
 library(ggplot2)
+library(igraph)
 
 ####################################
 ## Hasta la linea 193 son pruebas ##
@@ -189,13 +190,6 @@ Isolated = which(degree(G)==0)
 G2 = delete.vertices(G, Isolated)
 plot(G2, vertex.color='green', edge.curved = .1, vertex.size=20, vertex.label=indices[V(G2)], edge.label=rep(1, length(indices)-1))
 
-library(igraph)
-G <- graph_from_adjacency_matrix(M, mode=c('undirected'))
-Isolated = which(degree(G)==0)
-G2 = delete.vertices(G, Isolated)
-layout1 <- layout.fruchterman.reingold(g)
-plot(G2, edge.label=rep(1, length(indices)), vertex.color='green', edge.curved = .1)
-
 
 #E(G2)$pesos <- rep(1, length(E(G2)))
 df_deep_csv <- df_deep
@@ -204,6 +198,179 @@ df_deep_csv$ssdeep <- vapply(df_deep_csv$ssdeep, paste, collapse = ", ", charact
 
 
 
+
+G <- graph_from_adjacency_matrix(M, mode = 'undirected')
+Isolated = which(degree(G)==0)
+G2 = delete.vertices(G, Isolated)
+
+groupList <- list(g1 = c(3, 5),
+                  g2 = c(1, 7, 6),
+                  g3 = c(2,4))
+groupColours <- c(rgb(0,0.3,1,0.5), 
+                  rgb(0.8,0.4,0.1,0.5),  
+                  rgb(0.0,0.4,0.1,0.5))
+
+plot(G2, vertex.color='#ADD8E6', 
+     edge.curved = .1, 
+     vertex.size=20, 
+     vertex.label=indices[V(G2)], 
+     edge.label=rep(1, length(indices)-1),
+     mark.groups=groupList,
+     mark.col= groupColours
+)
+
+
+# Nos quedemos con los json del grafo
+indices
+df_reducido <- data.frame()
+for (i in indices) {
+  df_reducido <- rbind(df_reducido, df[i,])
+}
+View(df_reducido)
+
+ggplot(data=df_reducido, aes(x=df$submission.submitter_country)) + geom_histogram(binwidth = 0.5)
+
+# Todos vienen de California
+df_reducido %>% select(submission.submitter_country) %>% unique()
+
+# Todos tienen el mismo tamaño
+df_reducido %>% select(size) %>% unique()
+
+# Subidos todos en 10min
+df_reducido %>% select(first_seen, scan_date) %>% mutate(first_seen= gsub('20[0-9]{2}-[0-9]+-[0-9]+', '',first_seen), scan_date= gsub('20[0-9]{2}-[0-9]+-[0-9]+', '',scan_date)) %>% 
+                arrange(desc(.)) 
+# Mismo archivo subido 8 veces
+
+
+
+get_adj_matrix <- function(distancia) {
+  
+  indices <- c()
+  distancias <- c()
+  for (i in 1:nrow(df_deep)) {
+    for (j in (i+1):nrow(df_deep)) {
+      if ( adist(df_deep[i,], df_deep[j,]) <= distancia) {
+        indices <- c(indices, i,j)
+        distancias <- c(distancias, adist(df_deep[i,], df_deep[j,]))
+      }
+    }
+  }
+  
+  n <- nrow(df)
+  Mat <- matrix(0, nrow = n, ncol = n)
+  colnames(Mat) <- c(1:n)
+  row.names(Mat) <- c(1:n)
+  j <- 1
+  while(j < length(indices)) {
+    Mat[indices[j], indices[j+1]] <- 1
+    j <- j+2
+  }
+  print(distancias)
+  G <- graph_from_adjacency_matrix(Mat, mode = 'undirected')
+  G <- set_edge_attr(G, 'dist', value=distancias)
+  
+  return(G)
+  
+}
+
+G <- get_adj_matrix(10)
+#G <- simplify(G)
+Isolated = which(degree(G)==0)
+G2 = delete.vertices(G, Isolated)
+plot(G2, edge.label=E(G)$dist)
+
+
+
+c <- components(G)
+c$membership
+biggest <- which.max(c$csize)
+vids <- V(G)[c$membership==biggest]
+plot(induced_subgraph(G, vids), edge.label=E(G)$dist)
+
+vids <- V(G)[c$membership==biggest]
+plot(induced_subgraph(G, vids))
+plot(induced_subgraph(G, vids),vertex.size = 10, vertex.color = "blue", vertex.frame.color = '#123456', vertex.label.cex = .7,  vertex.label = NA, edge.curved = .5, edge.arrow.size = .3, edge.width = .7)
+
+G <- get_adj_matrix(10)
+
+c <- components(G)
+biggest <- which.max(c$csize)
+vids <- V(G)[c$membership==biggest]
+
+v <- as(vids, 'vector')
+
+compare <- data.frame()
+for (i in v) {
+  compare <- rbind(compare, df[i,])
+}
+View(compare)
+
+compare %>% select(size) %>% unique()
+compare %>% select(submission.submitter_country) %>% unique()
+
+times <- compare %>% select(first_seen, scan_date) %>% 
+  mutate(first_seen = gsub('20[0-9]{2}-[0-9]+-[0-9]+', '', first_seen), scan_date = gsub('20[0-9]{2}-[0-9]+-[0-9]+', '', scan_date)) %>% 
+  sort()
+
+head(times,1)
+tail(times,1)
+
+compare %>% select(positives) %>% lapply(., mean)
+compare %>% select(positives) %>% lapply(., sd)
+
+
+
+
+
+compare_p <- data.frame()
+for (i in v) {
+  compare_p <- rbind(compare_p, dfp[i,])
+}
+View(compare_p)
+
+
+
+get_results <- function(json) {
+  
+  json_data <- tidyjson::read_json(json)
+  df_temp <- json_data %>% 
+    gather_object() %>%   
+    filter(name=='scans') %>% 
+    spread_all() %>% 
+    gather_object() %>% 
+    select(ends_with('result')) %>% 
+    .[1,] %>% 
+    select(-last_col())
+  
+  
+  return(df_temp) 
+}
+
+ficheros <- sapply(vids, function(x) paste0(sprintf("%04d", x), '.json') )
+
+df_results <- data.frame()
+for (i in ficheros) {
+  print(paste0(path,i))
+  df_results <- rbind.fill(df_results, get_results(paste0(path,i)))
+}
+df_results <- df_results %>% select(-..JSON)
+colnames(df_results) <- lapply(colnames(df_results), function(x) gsub('.result', '', x))
+
+write.csv(df_results, "~/Documentos/LCC/ProyectoVT/Proyecto/escaneres.csv")
+
+df_results %>% select(everything()) %>%  filter(starts_with('not'))
+
+
+df_results %>% select(McAfee, Fortinet)
+
+df_results2 <- data.frame()
+for (i in nombres_ficheros) {
+  print(paste0(path,i))
+  df_results2 <- rbind.fill(df_results2, get_results(paste0(path,i)))
+}
+
+df_results2 <- df_results2[, !is.na(df_results2[1,])]
+df_results2 <- df_results2 %>% na.omit()
 
 ####################
 
